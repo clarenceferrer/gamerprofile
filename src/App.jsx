11aff4142c9febd726287976bc6c8333e7769613
andreii2404 <andreicarisma24@gmail.com>
2025-09-14 17:37:26 +0800

    Add QuiasProfile component with achievements and badges

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+// src/App.jsx
+import React from "react";
+import QuiasProfile from "./QuiasProfile";
+import "./App.css";
+
+export default function App() {
+  return <QuiasProfile />;
+}
---
+++ src/GamesData.jsx
@@ -0,0 +1,26 @@
+// src/gamesData.js
+
+// Import all game images
+import game1 from "./assets/genshin.jpg";
+import game2 from "./assets/call.jpg";
+import game3 from "./assets/star.png";
+import game4 from "./assets/hok.png";
+import game5 from "./assets/lol.png";
+import game6 from "./assets/manor.png";
+import game7 from "./assets/rocket.png";
+import game8 from "./assets/asphalt.jpg";
+import game9 from "./assets/rise.png";
+import game10 from "./assets/kahoot.jpg";
+
+export const ALL_GAMES = [
+  { id: 1, title: "Genshin Impact", category: "Adventure", img: game1 },
+  { id: 2, title: "Call of Duty: Mobile", category: "Shooting", img: game2 },
+  { id: 3, title: "Stardew Valley", category: "Paid", img: game3 },
+  { id: 4, title: "Honor of Kings", category: "Action", img: game4 },
+  { id: 5, title: "League of Legends", category: "Action", img: game5 },
+  { id: 6, title: "Mystery Manor", category: "Strategy", img: game6 },
+  { id: 7, title: "Rocket League Sideswipe", category: "Sports", img: game7 },
+  { id: 8, title: "Asphalt Legends", category: "Simulation", img: game8 },
+  { id: 9, title: "Rise of Kingdoms", category: "Strategy", img: game9 },
+  { id: 10, title: "Kahoot!", category: "Education", img: game10 },
+];
